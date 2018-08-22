515f7eb993e0ad6eecddfc04c02eebd5f91aff10
hongmaoxiao <769501294@qq.com>
2018-08-23 01:30:22 +0800

    add ValidAnagram

---
+++ JavaScript/242-ValidAnagram/validAnagram.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+const isAnagram = (s, t) => {
+    if (s.length !== t.length) {
+        return false;
+    }
+
+    const obj = {};
+
+    for (let i = 0; i < s.length; i++) {
+        if (!obj[s[i]]) {
+            obj[s[i]] = 1
+        } else {
+            obj[s[i]]++;            
+        }
+    }
+
+    for (let j = 0; j < t.length; j++) {
+        if (obj[t[j]]) {
+            obj[t[j]]--;
+            if (obj[t[j]] === 0) {
+                delete obj[t[j]];
+            }
+        }
+    }
+
+    return Object.keys(obj).length === 0;
+};
+
+console.log(isAnagram("anagram", "nagaram"));
