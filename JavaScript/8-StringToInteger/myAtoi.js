07aabca660ccbbc0328f5aaf61709d76f1170a2c
hongmaoxiao <769501294@qq.com>
2018-08-24 00:24:30 +0800

    add StringToInteger

--- JavaScript/8-StringToInteger/myAtoi.js
+++ JavaScript/8-StringToInteger/myAtoi.js
@@ -8,12 +8,12 @@ const myAtoi = (str) => {
 
     let flag = '+';
     let res = [];
-    str = str.replace(/[\s]/g,'');
+    str = str.trim();
 
     for (let i = 0; i < str.length; i++) {
         const el = str[i];
         if (i === 0) {
-            if (!el.match(/\d/)) {
+            if (!isNumber(el)) {
                 if (el === '-') {
                     flag = '-';
                 } else if (el === '+') {
@@ -25,23 +25,23 @@ const myAtoi = (str) => {
                 res.push(el);
             }
         } else {
-            if (!el.match(/\d/)) {
+            if (!isNumber(el)) {
                 break;
             } else {
-                res.push(el);
+                if (+el === 0 && res.length === 0) {
+                    continue;
+                } else {
+                    res.push(el);
+                }
             } 
         }
     }
 
     const len = res.length;
 
-    console.log('res: ', res);
-
-    if (len === 0 || (len > 0 && +res[0] === 0)) {
+    if (len === 0) {
         return 0;
     }
-
-    console.log('res next: ', res);
 
     res = res.reduce((acc, o, i) => {
         acc += o * Math.pow(10, len - i - 1);
@@ -49,14 +49,13 @@ const myAtoi = (str) => {
     }, 0);
 
     res = Number(`${flag}${res}`);
-
     return res > up ? up : ((res < down) ? down : res);
 };
 
-// myAtoi("42");
-// myAtoi("   -42");
-// myAtoi("4193 with words");
-// myAtoi("words and 987");
-// myAtoi("-91283472332");
-myAtoi("   +0 123");
-myAtoi("  0000000000012345678");
+const isNumber = (s) => {
+    if (s === ' ') {
+        return false;
+    }
+    const toNumber = Number(s);
+    return toNumber === toNumber;
+}
--- JavaScript/8-StringToInteger/myAtoi1.js
+++ JavaScript/8-StringToInteger/myAtoi1.js
@@ -3,60 +3,33 @@
  * @return {number}
  */
 const myAtoi = (str) => {
-    const up = Math.pow(2, 31) - 1;
-    const down = -Math.pow(2, 31);
+    let i = 0;
+    let res = 0;
+    let isNegative = false;
 
-    let flag = '+';
-    let res = [];
-    str = str.replace(/[\s]/g,'');
+    while (str[i] === ' ') {
+        i++;
+    }
 
-    for (let i = 0; i < str.length; i++) {
-        const el = str[i];
-        if (i === 0) {
-            if (!el.match(/\d/)) {
-                if (el === '-') {
-                    flag = '-';
-                } else if (el === '+') {
-                    flag = '+';
-                } else {
-                    return 0;
-                }
-            } else {
-                res.push(el);
-            }
-        } else {
-            if (!el.match(/\d/)) {
-                break;
-            } else {
-                res.push(el);
-            } 
+    const sign = str[i];
+    if (sign === '+' || sign === '-') {
+        isNegative = sign === '-';
+        i++;
+    }
+
+    for (; i < str.length; i++) {
+        const code = str.charCodeAt(i) - 48;
+        if (code < 0 || code > 9) {
+            break;
         }
+        
+        res *= 10;
+        res += code;
     }
 
-    const len = res.length;
-
-    console.log('res: ', res);
-
-    if (len === 0 || (len > 0 && +res[0] === 0)) {
-        return 0;
+    if (isNegative) {
+        res = -res;
     }
 
-    console.log('res next: ', res);
-
-    res = res.reduce((acc, o, i) => {
-        acc += o * Math.pow(10, len - i - 1);
-        return acc;
-    }, 0);
-
-    res = Number(`${flag}${res}`);
-
-    return res > up ? up : ((res < down) ? down : res);
+    return Math.max(-(2**31), Math.min(2**31 - 1, res));
 };
-
-// myAtoi("42");
-// myAtoi("   -42");
-// myAtoi("4193 with words");
-// myAtoi("words and 987");
-// myAtoi("-91283472332");
-myAtoi("   +0 123");
-myAtoi("  0000000000012345678");
