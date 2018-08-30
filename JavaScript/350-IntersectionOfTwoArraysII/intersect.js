02561d75748790eb0c816509543b2f0ac66b1c8e
fengxiaomao <fxm@autoforce.net>
2018-08-30 20:48:17 +0800

    add part of IntersectionOfTwoArraysII

---
+++ JavaScript/350-IntersectionOfTwoArraysII/intersect.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+// Input: nums1 = [1,2,2,1], nums2 = [2,2]
+// Output: [2,2]
+const intersect = (nums1, nums2) => {
+    
+};
