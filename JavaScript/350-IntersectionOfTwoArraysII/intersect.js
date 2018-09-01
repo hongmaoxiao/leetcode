6c2ee54b203b9a55e39e19776226499f5cbcb6ca
fengxiaomao <fxm@autoforce.net>
2018-09-01 12:03:52 +0800

    add IntersectionOfTwoArraysII

--- JavaScript/350-IntersectionOfTwoArraysII/intersect.js
+++ JavaScript/350-IntersectionOfTwoArraysII/intersect.js
@@ -3,8 +3,35 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-// Input: nums1 = [1,2,2,1], nums2 = [2,2]
-// Output: [2,2]
 const intersect = (nums1, nums2) => {
-    
+   if (nums1.length === 0 || nums2.length === 0) {
+       return [];
+   }
+
+   const res = [];
+   for (let i = 0; i < nums1.length; i++) {
+       if (nums2.length === 0) {
+           break;
+       }
+       const ele = nums1[i];
+       if (findSame(nums2, ele)) {
+           res.push(ele);
+       }
+   }
+
+   return res;
 };
+
+const findSame = (arr, num) => {
+    let find = false;
+    for (let i = 0; i < arr.length; i++) {
+        const ele = arr[i];
+        if (ele === num) {
+            find = true;
+            arr.splice(i, 1);
+            i--;
+            break;
+        }
+    }
+    return find;
+}
