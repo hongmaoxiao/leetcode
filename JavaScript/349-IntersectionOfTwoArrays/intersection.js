afe867662ac974c88158c758be10e1ab33dd5626
fengxiaomao <fxm@autoforce.net>
2018-09-01 12:15:51 +0800

    add IntersectionOfTwoArrays

---
+++ JavaScript/349-IntersectionOfTwoArrays/intersection.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+const intersect = (nums1, nums2) => {
+   if (nums1.length === 0 || nums2.length === 0) {
+       return [];
+   }
+
+   const res = [];
+   for (let i = 0; i < nums1.length; i++) {
+       if (nums2.length === 0) {
+           break;
+       }
+       const ele = nums1[i];
+       if (findSame(nums2, ele)) {
+           res.push(ele);
+       }
+   }
+
+   return res;
+};
+
+const findSame = (arr, num) => {
+    let find = false;
+    for (let i = 0; i < arr.length; i++) {
+        const ele = arr[i];
+        if (ele === num) {
+            find = true;
+            arr.splice(i, 1);
+            i--;
+            break;
+        }
+    }
+    return find;
+}
--- JavaScript/350-IntersectionOfTwoArraysII/intersect.js
+++ JavaScript/350-IntersectionOfTwoArraysII/intersect.js
@@ -3,23 +3,26 @@
  * @param {number[]} nums2
  * @return {number[]}
  */
-const intersect = (nums1, nums2) => {
-   if (nums1.length === 0 || nums2.length === 0) {
-       return [];
-   }
+const intersection = (nums1, nums2) => {
+    if (nums1.length === 0 || nums2.length === 0) {
+        return [];
+    }
 
-   const res = [];
-   for (let i = 0; i < nums1.length; i++) {
-       if (nums2.length === 0) {
-           break;
-       }
-       const ele = nums1[i];
-       if (findSame(nums2, ele)) {
-           res.push(ele);
-       }
-   }
+    const res = [];
+    for (let i = 0; i < nums1.length; i++) {
+        if (nums2.length === 0) {
+            break;
+        }
+        const ele = nums1[i];
+        if (res.indexOf(ele) > -1) {
+            continue;
+        }
+        if (findSame(nums2, ele)) {
+            res.push(ele);
+        }
+    }
 
-   return res;
+    return res;
 };
 
 const findSame = (arr, num) => {
@@ -30,7 +33,6 @@ const findSame = (arr, num) => {
             find = true;
             arr.splice(i, 1);
             i--;
-            break;
         }
     }
     return find;
