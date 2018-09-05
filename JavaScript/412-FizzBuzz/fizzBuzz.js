effb736f5f590b72e7f80acee28f28c3ffea478b
hongmaoxiao <769501294@qq.com>
2018-09-05 22:37:39 +0800

    add FizzBuzz

---
+++ JavaScript/412-FizzBuzz/fizzBuzz.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} n
+ * @return {string[]}
+ */
+const fizzBuzz = (n) => {
+    const res = Array(n);
+    for (let i = 1; i <= n; i++) {
+        if (i % 15 === 0) {
+            res[i - 1] = "FizzBuzz";
+        } else if (i % 3 === 0) {
+            res[i - 1] = "Fizz";
+        } else if (i % 5 === 0) {
+            res[i - 1] = "Buzz";
+        } else {
+            res[i - 1] = '' + i;
+        }
+    }
+
+    return res;
+};
