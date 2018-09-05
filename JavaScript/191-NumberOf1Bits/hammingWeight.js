ce57df9b5cc46a090e7c6649b4b577874346aa6f
hongmaoxiao <769501294@qq.com>
2018-09-06 00:12:48 +0800

    add NumberOf1Bits

---
+++ JavaScript/191-NumberOf1Bits/hammingWeight.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n - a positive integer
+ * @return {number}
+ */
+const hammingWeight = (n) => {
+    let count = 0;
+    while (n > 0) {
+        const mod = n % 2;
+        if (mod === 1) {
+            count++;
+        }
+        n = Math.floor(n / 2);
+    }
+
+    console.log('count: ', count);
+
+    return count;
+};
