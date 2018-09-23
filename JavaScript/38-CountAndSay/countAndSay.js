85cddd49a7f94990951506a16c7c897f8dbbe22d
fengxiaomao <fxm@autoforce.net>
2018-09-23 21:35:22 +0800

    forget to return value

--- JavaScript/38-CountAndSay/countAndSay.js
+++ JavaScript/38-CountAndSay/countAndSay.js
@@ -2,17 +2,14 @@
  * @param {number} n
  * @return {string}
  */
-// 1.     1
-// 2.     11
-// 3.     21
-// 4.     1211
-// 5.     111221
 const countAndSay = (n) => {
     let res = '1';
 
     while (--n) {
         res = countNext(res);
     }
+
+    return res;
 };
 
 const countNext = (str) => {
