aabbefb6e0dc5032579d55255c1176244aa9b70c
fengxiaomao <fxm@autoforce.net>
2018-10-03 00:19:02 +0800

    fix StringCompression

--- JavaScript/443-StringCompression/compress.js
+++ JavaScript/443-StringCompression/compress.js
@@ -7,7 +7,7 @@ const compress = (chars) => {
     let write = 0;
 
     for (let read = 0; read < chars.length; read++) {
-        if (read + 1 !== chars.length || chars[read + 1] !== chars[read]) {
+        if (read + 1 === chars.length || chars[read + 1] !== chars[read]) {
             chars[write++] = chars[anchor];
 
             if (read > anchor) {
