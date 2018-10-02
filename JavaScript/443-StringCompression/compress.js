9d4d3afdf3c112aea9ef8781025345079cec9f23
fengxiaomao <fxm@autoforce.net>
2018-10-03 00:14:33 +0800

    add StringCompression

---
+++ JavaScript/443-StringCompression/compress.js
@@ -0,0 +1,25 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+const compress = (chars) => {
+    let anchor = 0;
+    let write = 0;
+
+    for (let read = 0; read < chars.length; read++) {
+        if (read + 1 !== chars.length || chars[read + 1] !== chars[read]) {
+            chars[write++] = chars[anchor];
+
+            if (read > anchor) {
+                const str = (read - anchor + 1).toString();
+                for (let c = 0; c < str.length; c++) {
+                    chars[write++] = str[c];
+                }
+            }
+
+            anchor = read + 1;
+        }
+    }
+
+    return write;
+};
